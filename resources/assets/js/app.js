7b46a945e539fc58b29ca79a99308bf7fb29fab3
JoseGopar <cuentacaraalpublico@gmail.com>
2018-03-21 13:58:15 +0000

    Intallacion CSS y Js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,3 +1,5 @@
+import Buefy from 'buefy';
+
 
 /**
  * First we will load all of this project's JavaScript dependencies which
@@ -9,14 +11,13 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+Vue.use(Buefy);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
